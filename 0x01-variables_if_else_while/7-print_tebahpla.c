3578fe5f68b5d30b8abfaa4863654b9c79305221
CharityVaati <charitykilonzo96@gmail.com>
2022-09-09 03:48:49 -0700

    7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
-#include <ctype.h>
 /**
-*main - Program to print alphabet letters in reverse followed by new line
-*
-*Return: return 0
-*/
+ *main - entry point
+ *
+ *Description:prints alphabet in reverse
+ *
+ *Return: Always zero
+ */
 int main(void)
 {
-int letter;
+int letter, n;
 
-for (letter = z; letter >= a; letter--)
+for (letter = 122, n = 1; n < 27; letter--, n++)
 {
 putchar(letter);
 }
-putchar(n);
+putchar(10);
 return (0);
 }
