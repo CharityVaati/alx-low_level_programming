e86975172d946d7cafc21e193f455b58576548fd
CharityVaati <charitykilonzo96@gmail.com>
2022-09-09 02:49:33 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
+#include <stdlib.h>
 /**
- *main - Entry point
- *Description: Print all single digit numbers of base 10
- *stating from 0, followed by a new line
- *Return: 0
+ * main - main block
+ * Description: prints all single digit numbers of base 10
+ * starting from 0, followed by a new line.
+ * Return: 0
  */
 int main(void)
 {
