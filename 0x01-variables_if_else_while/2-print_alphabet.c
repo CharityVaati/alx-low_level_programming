d81bba9010e0a95bde913c74bf7af20091154f3c
CharityVaati <charitykilonzo96@gmail.com>
2022-09-09 03:01:58 -0700

    Printalphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,17 +1,20 @@
 #include <stdio.h>
 
 /**
- * main - Prints the alphabetic
- *
- * Return: Always (Success)
+ * main - assign a random number to the variable n each time it is  executed
+ * and print the last digit of the number stored in the variable n
+ * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-char c;
+int ch;
 
-for (c = a; c <= z; c++)
-putchar(c);
+for (ch = 97; ch <= 122; ch++)
+{
+putchar(ch);
+}
+putchar(10); /* this is an ASCII code for new line */
 
-putchar(n);
 return (0);
 }
