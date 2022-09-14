31bd97b1d7f0b1ab6ad2e33ae59138d616098150
CharityVaati <charitykilonzo96@gmail.com>
2022-09-13 21:08:14 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,26 +1,23 @@
-#include "main.h"
+#include"main.h"
 
 /**
- * jack_bauer - prints every minute of the day of Jack Bauer
- * starting from 00:00 to 23:59
- */
+ * jack_bauer- prints every minute of the day
+*/
+
 void jack_bauer(void)
 {
-int i, j;
-i = 0;
-while (i < 24)
+int min, hr;
+
+for (hr = 0; hr <= 23; ++hr)
 {
-j = 0;
-while (j < 60)
+for (min = 0; min <= 59; ++min)
 {
-_putchar((i / 10) + 0);
-_putchar((i % 10) + 0);
+_putchar((hr / 10) + 48);
+_putchar((hr % 10) + 48);
 _putchar(:);
-_putchar((j / 10) + 0);
-_putchar((j % 10) + 0);
+_putchar((min / 10) + 48);
+_putchar((min % 10) + 48);
 _putchar(n);
-j++;
 }
-i++;
 }
 }
