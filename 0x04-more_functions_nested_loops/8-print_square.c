d6244e9db1041802b206e6215bc8b4aedea27c8d
CharityVaati <charitykilonzo96@gmail.com>
2022-09-15 04:16:13 -0700

    8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,24 +1,25 @@
 #include "main.h"
-/**
-*print_square - prints squares
-*@size: parameter
-*Return:returns nothing
-*/
-void print_square(int size)
-{
-int inc1, inc2;
-if (size > 0)
-{
-for (inc1 = 0; inc1 < size; inc1++)
-{
-for (inc2 = 0; inc2 < (size - 1); inc2++)
-_putchar(#);
+#include <stdio.h>
 
+/**
+ * print_square - Print square line dependent on the integer n.
+ * @n : The number of lines using # characters to use per row and column
+ * Return: Void.
+ */
+void print_square(int n)
+{
+int x;
+int y;
+
+for (y = 0; y < n; y++)
+{
+for (x = 0; x < n; x++)
+{
 _putchar(#);
+}
 _putchar(n);
 }
-}
-else
+if (n <= 0)
 {
 _putchar(n);
 }
